2ebe54a5b4be10d6a59adb49583240b7a356fbd2
Dipak Poudel <pdl.dipak@gmail.com>
2021-07-14 09:41:40 +0200

    bug fix to render image

--- src/features/map/components/MapCallout.js
+++ src/features/map/components/MapCallout.js
@@ -2,7 +2,7 @@ import React from 'react';
 import CompactRestaurantInfo from '../../restaurants/components/compactRestaurantInfo/CompactRestaurantInfo';
 
 const MapCallout = ({ restaurant }) => {
-  return <CompactRestaurantInfo restaurant={restaurant} />;
+  return <CompactRestaurantInfo restaurant={restaurant} isMap/>;
 };
 
 export default MapCallout;
